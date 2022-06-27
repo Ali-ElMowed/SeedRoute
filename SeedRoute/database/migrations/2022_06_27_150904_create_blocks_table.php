26804f444db3bbf5c379bf148db17d4ef725e43a
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-27 19:14:56 +0300

    Created the database tables

---
+++ SeedRoute/database/migrations/2022_06_27_150904_create_blocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->integer('user_id');
+            $table->integer('plant_id');
+            $table->integer('phase');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+};
