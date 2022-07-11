d2fd5a999fadaa9d4eecda88a4d67760ed335f22
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-07-11 13:32:29 +0300

    Backend: Created get plant by id or all plants API

--- SeedRoute/app/Http/Controllers/Controller.php
+++ SeedRoute/app/Http/Controllers/Controller.php
@@ -6,8 +6,30 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Plant;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //function to get a targeted plant by ID or all plants
+    public function getPlants($id = null){
+        if($id != null){
+            $plants = Plant::find($id);
+        }
+        else{
+            $plants = Plant::all();
+        }
+
+        return self::returnResponse("plants found",200,$plants);
+    }
+
+    //function to be used in all functions for response return
+    public function returnResponse($status,$code,$data = null)
+    {
+        return response()->json([
+            'status'=>$status,
+            'data' => $data     
+        ],$code);
+    }
 }
