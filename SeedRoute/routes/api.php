b91f992eb63fceda77e9f9328078472d8d0f0937
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-07-11 14:53:18 +0300

    Backedn: Created the JWT

--- SeedRoute/routes/api.php
+++ SeedRoute/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlantController;
+use App\Http\Controllers\JWTController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,13 @@ use App\Http\Controllers\PlantController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group(['middleware' => 'api'], function($router) {
+    Route::post('/register', [JWTController::class, 'register']);
+    Route::post('/login', [JWTController::class, 'login']);
+    Route::post('/logout', [JWTController::class, 'logout']);
+    Route::post('/refresh', [JWTController::class, 'refresh']);
+    Route::post('/profile', [JWTController::class, 'profile']);
 });
 
 Route::get('/getPlants/{id?}',[PlantController::class,'getPlants']);
