1141e0beabea110d0d2c6d0050f6a2f5f580673f
ali-elmowed <almowed.ali@gmail.com>
2022-07-21 21:52:56 +0300

    Frontend: Inserted bottomTabnav to homescreen

--- seedroute!frontend/app/screens/home/home.screen.tsx
+++ seedroute!frontend/app/screens/home/home.screen.tsx
@@ -1,57 +1,18 @@
 import React from "react";
-import { Text, View, StyleSheet, Button, ScrollView } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Colors } from "react-native/Libraries/NewAppScreen";
-import Block from "../../Components/Block";
-import Btn from "../../Components/Btn";
+import { Button, Text, View } from "react-native";
+import BottomTabNavigator from "../../bottomTab.navigator";
 
-interface homeScreenProps {
+interface loginScreenProps {
   navigation: any;
 }
-const Home = (props: homeScreenProps) => {
-  const goBackk = () => props.navigation.goBack();
-  const myLand = () => props.navigation.navigate("MyLand");
 
-    const arr:any = []
-    for(let i = 0 ; i <28 ; i++){
-        arr.push(
-            <Block id={i}/>
-        )
-    }
+function HomeScreen(props: loginScreenProps) {
+  const goSketch = () => props.navigation.navigate("Sketch");
   return (
-    <ScrollView>
-      <View >
-        <Text style={styles.titleTxt}>Sketch Your Land</Text>
-        <View style={styles.container}>{arr}</View>
-        
-        {/* <Button title="BACK" onPress={goBackk}></Button> */}
-        <Btn text="Next" onPress={myLand} style={styles.nxtBtn}/>
-      </View>
-    </ScrollView>
+
+    <BottomTabNavigator />
+
   );
-};
-const styles = StyleSheet.create({
-    container:{
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent:'center',
-        alignContent:'center',
-        marginBottom:7
-    },
-    nxtBtn:{
-        marginLeft:100,
-        marginRight:100,
-        color:"white"
-    },
-    titleTxt:{
-        margin:10,
-        marginBottom:13,
-        alignSelf:'center',
-        fontSize:32,
-        fontWeight:'bold',
-        
-    }
+}
 
-  });
-
-export default Home;
+export default HomeScreen;
