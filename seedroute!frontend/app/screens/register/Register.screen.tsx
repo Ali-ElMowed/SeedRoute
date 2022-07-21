df10a770fa4c57aadb27576cec33e984d3e2c4cb
ali-elmowed <almowed.ali@gmail.com>
2022-07-21 11:33:30 +0300

    Frontend: Created the register screen

---
+++ seedroute!frontend/app/screens/register/Register.screen.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+
+interface RegisterScreenProps {
+    navigation: any;
+  }
+  
+  const Register = (props: RegisterScreenProps) => {
+  
+    return (
+      <View>
+        <Text>Register Screen</Text> 
+      </View>
+    );
+  };
+  
+  export default Register;
+  
