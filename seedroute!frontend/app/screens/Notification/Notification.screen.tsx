85621c1a8cc69e5e27e70bc80f7b12a70eb0e19c
ali-elmowed <almowed.ali@gmail.com>
2022-07-21 22:26:05 +0300

    Frontend: Created the notification screen

---
+++ seedroute!frontend/app/screens/Notification/Notification.screen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+import Block from "../../Components/Block";
+import Btn from "../../Components/Btn";
+
+interface homeScreenProps {
+  navigation: any;
+}
+const Notification = (props: homeScreenProps) => {
+
+  return (
+    <ScrollView>
+      <View >
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+export default Notification;
