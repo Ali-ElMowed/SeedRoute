c77af5d1be1a6ad73dc8c0ddba14b6e27c11bfa9
ali-elmowed <almowed.ali@gmail.com>
2022-07-21 22:26:43 +0300

    Frontend: Added the Profile screen to navigation

--- seedroute!frontend/app/bottomTab.navigator.tsx
+++ seedroute!frontend/app/bottomTab.navigator.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profile from "./screens/Profile/Profile.screen";
+import Notification from "./screens/Notification/Notification.screen";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => (
     <Tab.Navigator >
         {/* <Tab.Screen name="Sketch" component={Sketch} options={{headerShown:false}} /> */}
-        {/* <Tab.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/> */}
-        <Tab.Screen name="Profile" component={Profile} options={{headerShown:false}}/>
+        <Tab.Screen name="Notification" component={Notification} options={{headerShown:false}}/>
+        <Tab.Screen name="Profile" component={Profile} options={{headerShown:false, } }/>
         {/* <Tab.Screen name="Home" component={HomeScreen}/> */}
     </Tab.Navigator>
 );
