06715f864897a9357bf82cbf68d10178fcdaaca1
ali-elmowed <almowed.ali@gmail.com>
2022-07-22 18:34:01 +0300

    Frontend: Created the Plant screen

---
+++ seedroute!frontend/app/screens/Plant/Plant.screen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+interface homeScreenProps {
+  navigation: any;
+}
+const Plant = (props: homeScreenProps) => {
+
+  return (
+    <ScrollView>
+      <View >
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+export default Plant;
