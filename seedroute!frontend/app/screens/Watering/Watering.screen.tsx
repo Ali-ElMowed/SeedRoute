d9b22657a3e1bcc287273fc03e064fbf832244a7
ali-elmowed <almowed.ali@gmail.com>
2022-07-22 22:14:27 +0300

    Frontend: Created the watering screen

---
+++ seedroute!frontend/app/screens/Watering/Watering.screen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+interface homeScreenProps {
+  navigation: any;
+}
+const Watering = (props: homeScreenProps) => {
+
+  return (
+    <ScrollView>
+      <View >
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+export default Watering;
