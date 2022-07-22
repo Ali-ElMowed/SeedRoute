b519efee001808791b06187a7ddf641fe4dabfc2
ali-elmowed <almowed.ali@gmail.com>
2022-07-22 19:02:38 +0300

    Frontend: Styled the image of the plant + name

--- seedroute!frontend/app/screens/Plant/Plant.screen.tsx
+++ seedroute!frontend/app/screens/Plant/Plant.screen.tsx
@@ -8,12 +8,27 @@ const Plant = (props: homeScreenProps) => {
   return (
     <ScrollView>
       <View >
+        <Image source={require("../../../assets/images/potato.jpg")} style={styles.img}/>
+        <Text style={styles.plant_name}>Potato</Text>
       </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-
+    img:{
+        maxWidth:250,
+        maxHeight:250,
+        resizeMode:'contain',
+        alignSelf:'center',
+        borderRadius:6,
+        marginTop:40
+    },
+    plant_name:{
+        textAlign:'center',
+        margin:20,
+        fontSize:32,
+        fontWeight:'bold'
+    }
 })
 export default Plant;
