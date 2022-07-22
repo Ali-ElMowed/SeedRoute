a5bd8f5d4b52589cea6a7ed65d21757361123059
ali-elmowed <almowed.ali@gmail.com>
2022-07-22 18:27:40 +0300

    Frontend: Added the screens to the nav tab

--- seedroute!frontend/app/bottomTab.navigator.tsx
+++ seedroute!frontend/app/bottomTab.navigator.tsx
@@ -4,17 +4,22 @@ import Profile from "./screens/Profile/Profile.screen";
 import Notifications from "./screens/Notification/Notification.screen";
 import Chats from "./screens/Chat/Chat.screen";
 import Plants from "./screens/Plants/Plants.screen";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {faMessage, faSeedling, faBell,faTableCells } from '@fortawesome/free-solid-svg-icons';
+
 
 const Tab = createBottomTabNavigator();
 
+
 const BottomTabNavigator = () => (
+    
     <Tab.Navigator 
     initialRouteName="Profile"
     >
-        <Tab.Screen name="Plants" component={Plants} options={{headerShown:false}} />
-        <Tab.Screen name="Chats" component={Chats} options={{headerShown:false}} />
-        <Tab.Screen name="Notifications" component={Notifications} options={{headerShown:false}}/>
-        <Tab.Screen name="Profile" component={Profile} options={{headerShown:false, } } />
+        <Tab.Screen name="Plants" component={Plants} options={{headerShown:false, tabBarIcon:()=>(<FontAwesomeIcon icon={faSeedling} style={{color:'#0D9B81', padding:12}}/>)}} />
+        <Tab.Screen name="Chats" component={Chats} options={{headerShown:false, tabBarIcon:()=>(<FontAwesomeIcon icon={faMessage} style={{color:'#0D9B81', padding:12}}/>)}} />
+        <Tab.Screen name="Notifications" component={Notifications} options={{headerShown:false, tabBarIcon:()=>(<FontAwesomeIcon icon={faBell} style={{color:'#0D9B81', padding:12}}/>)}}/>
+        <Tab.Screen name="Profile" component={Profile} options={{headerShown:false, tabBarIcon:()=>(<FontAwesomeIcon icon={faTableCells} style={{color:'#0D9B81', padding:12}}/>) } } />
     </Tab.Navigator>
 );
 
