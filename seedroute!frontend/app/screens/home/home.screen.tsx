047908cc0576f58a5e5f284194ad32ba43716cbf
ali-elmowed <almowed.ali@gmail.com>
2022-07-25 12:18:20 +0300

    Frontend: Changed the path of the navigation

--- seedroute!frontend/app/screens/home/home.screen.tsx
+++ seedroute!frontend/app/screens/home/home.screen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
-import BottomTabNavigator from "../../bottomTab.navigator";
+import BottomTabNavigator from "../../Navigations/bottomTab.navigator";
 
 interface loginScreenProps {
   navigation: any;
