20f2b103b2d3d510cceb28932b13505bf53d09ff
ali-elmowed <almowed.ali@gmail.com>
2022-07-25 17:27:20 +0300

    Frontend: Changes the path of the store

--- seedroute!frontend/App.tsx
+++ seedroute!frontend/App.tsx
@@ -7,7 +7,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from "./app/screens/login/login.screen";
 import AppNavigator from './app/Navigations/app.navigator';
 import { Provider } from 'react-redux';
-import store from './app/redux/store'
+import {store} from './app/redux/store'
 
 
 export default function App() {
