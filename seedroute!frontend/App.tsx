b4f6870549c8d268d059040d9723d1cf1faad6e7
ali-elmowed <almowed.ali@gmail.com>
2022-07-25 12:17:07 +0300

    Frontend: changes the path of the appnavigator

--- seedroute!frontend/App.tsx
+++ seedroute!frontend/App.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from "./app/screens/login/login.screen";
-import AppNavigator from './app/app.navigator';
+import AppNavigator from './app/Navigations/app.navigator';
 
 export default function App() {
 
