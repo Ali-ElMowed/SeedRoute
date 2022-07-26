876f8ae3bee76e4f123a6fc97f5be7ea7cf80a52
ali-elmowed <almowed.ali@gmail.com>
2022-07-26 15:26:26 +0300

    Frontend: Added axios funx to get plany by id

--- seedroute!frontend/app/Api/plants.tsx
+++ seedroute!frontend/app/Api/plants.tsx
@@ -4,3 +4,8 @@ export const plants = async () => {
     const res = await axios.get("http://10.0.2.2:8000/api/getPlants");
     return res;
 }
+
+export const getPlantById = async (data: number) => {
+    const res = await axios.get(`http://10.0.2.2:8000/api/getPlants/${data}`);
+    return res;
+}
