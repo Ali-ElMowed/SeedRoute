91b6e2615f3f253bd8c3da2fb48a057309467d61
ali-elmowed <almowed.ali@gmail.com>
2022-07-28 11:51:55 +0300

    Frontend: Wrap the appnav into paper provider

--- seedroute!frontend/App.tsx
+++ seedroute!frontend/App.tsx
@@ -1,21 +1,15 @@
-
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from "./app/screens/login/login.screen";
-import AppNavigator from './app/Navigations/app.navigator';
-import { Provider } from 'react-redux';
-import {store} from './app/redux/store'
-
+import * as React from "react";
+import AppNavigator from "./app/Navigations/app.navigator";
+import { Provider } from "react-redux";
+import { store } from "./app/redux/store";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
-
   return (
     <Provider store={store}>
-      <AppNavigator/>
+      <PaperProvider >
+        <AppNavigator />
+      </PaperProvider>
     </Provider>
   );
 }
-
