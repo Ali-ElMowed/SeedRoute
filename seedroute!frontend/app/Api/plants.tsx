65ee35efd604878b12caf35a05057a741c5ce8ef
ali-elmowed <almowed.ali@gmail.com>
2022-07-29 19:58:50 +0300

    Frontend: Used async storage instead of local

--- seedroute!frontend/app/Api/plants.tsx
+++ seedroute!frontend/app/Api/plants.tsx
@@ -1,22 +1,24 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { baseURL } from "./base";
 console.log(localStorage.getItem('user'));
 
-const user = JSON?.parse(localStorage.getItem('user') || '{}')?.access_token
 
 export const plants = async () => {
+ const user:any = JSON.parse(await AsyncStorage.getItem('user')||'')
     const res = await axios.get(`http://10.0.2.2:8000/api/getPlants`,{
         headers: {
-            Authorization :  `Bearer ${user}`
+            Authorization :  `Bearer ${user?.access_token}`
         }
     });
     return res;
 }
 
 export const getPlantById = async (data: number) => {
+ const user:any = JSON.parse(await AsyncStorage.getItem('user')||'')
     const res = await axios.get(`http://10.0.2.2:8000/api/getPlants/${data}`,{
         headers: {
-            Authorization :  `Bearer ${user}`
+            Authorization :  `Bearer ${user?.access_token}`
         }
     });
     return res;
