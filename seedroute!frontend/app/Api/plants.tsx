272cfa5deb0cd96dc89ccee261547f5da8b22636
ali-elmowed <almowed.ali@gmail.com>
2022-07-29 17:34:51 +0300

    Frontend: Created the plants apis with auth

--- seedroute!frontend/app/Api/plants.tsx
+++ seedroute!frontend/app/Api/plants.tsx
@@ -1,11 +1,23 @@
 import axios from "axios";
+import { baseURL } from "./base";
+console.log(localStorage.getItem('user'));
+
+const user = JSON?.parse(localStorage.getItem('user') || '{}')?.access_token
 
 export const plants = async () => {
-    const res = await axios.get("http://10.0.2.2:8000/api/getPlants");
+    const res = await axios.get(`http://10.0.2.2:8000/api/getPlants`,{
+        headers: {
+            Authorization :  `Bearer ${user}`
+        }
+    });
     return res;
 }
 
 export const getPlantById = async (data: number) => {
-    const res = await axios.get(`http://10.0.2.2:8000/api/getPlants/${data}`);
+    const res = await axios.get(`http://10.0.2.2:8000/api/getPlants/${data}`,{
+        headers: {
+            Authorization :  `Bearer ${user}`
+        }
+    });
     return res;
 }
