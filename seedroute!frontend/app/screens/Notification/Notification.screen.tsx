cc5919b4d88e8abb101d34d56568892bfeda30c3
ali-elmowed <almowed.ali@gmail.com>
2022-07-29 13:44:24 +0300

    Frontend: Styled the notification screen

--- seedroute!frontend/app/screens/Notification/Notification.screen.tsx
+++ seedroute!frontend/app/screens/Notification/Notification.screen.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+import { Text, View, StyleSheet, Image, ScrollView, Pressable } from "react-native";
+import { faFaucetDrip } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+
 interface homeScreenProps {
   navigation: any;
 }
@@ -8,12 +11,34 @@ const Notifications = (props: homeScreenProps) => {
   return (
     <ScrollView>
       <View >
+        
+        
+        <Pressable  style={styles.chatBar}>
+          <FontAwesomeIcon icon={faFaucetDrip} style={styles.icon} />
+          <View>
+          <Text style={styles.text}>Watering has just started.</Text>
+          </View>
+        </Pressable>
       </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-
+  icon: {
+    color: "#0D9B81",
+    margin:30,
+    padding: 14,
+    
+  },
+  chatBar: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    flexDirection: "row",
+  },
+  text:{
+    fontSize: 22,
+    marginTop: 25
+  }
 })
 export default Notifications;
