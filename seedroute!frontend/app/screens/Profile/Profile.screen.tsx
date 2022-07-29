19b0ac83f7da8fe07cd8b2867f568b0f23065507
ali-elmowed <almowed.ali@gmail.com>
2022-07-29 19:54:51 +0300

    Frontend: Created the profile blocks as fetched

--- seedroute!frontend/app/screens/Profile/Profile.screen.tsx
+++ seedroute!frontend/app/screens/Profile/Profile.screen.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+import { getSelectedBlocks } from "../../Api/blocks";
 import Block from "../../Components/Block";
 import Btn from "../../Components/Btn";
+import Loading from "../../Components/Loading";
 import ProfileBlocks from "../../Components/ProfileBlocks";
 
 interface homeScreenProps {
@@ -9,83 +12,228 @@ interface homeScreenProps {
 }
 const Profile = (props: homeScreenProps) => {
   const goToSketch = () => props.navigation.navigate("Sketch");
-  const goEditProfile = () =>{ props.navigation.navigate("Sketch")
-
-};
+  const goEditProfile = () => {
+    props.navigation.navigate("Sketch");
+  };
+  const [user,setUser] = useState('')
+  const [loading, setLoading] = useState(false);
+  const [blocks, setBlocks]: any = useState([
+    {
+      name: 0,
+      value: 0,
+    },
+    {
+      name: 1,
+      value: 0,
+    },
+    {
+      name: 2,
+      value: 0,
+    },
+    {
+      name: 3,
+      value: 0,
+    },
+    {
+      name: 4,
+      value: 0,
+    },
+    {
+      name: 5,
+      value: 0,
+    },
+    {
+      name: 6,
+      value: 0,
+    },
+    {
+      name: 7,
+      value: 0,
+    },
+    {
+      name: 8,
+      value: 0,
+    },
+    {
+      name: 9,
+      value: 0,
+    },
+    {
+      name: 10,
+      value: 0,
+    },
+    {
+      name: 11,
+      value: 0,
+    },
+    {
+      name: 12,
+      value: 0,
+    },
+    {
+      name: 13,
+      value: 0,
+    },
+    {
+      name: 14,
+      value: 0,
+    },
+    {
+      name: 15,
+      value: 0,
+    },
+    {
+      name: 16,
+      value: 0,
+    },
+    {
+      name: 17,
+      value: 0,
+    },
+    {
+      name: 18,
+      value: 0,
+    },
+    {
+      name: 19,
+      value: 0,
+    },
+    {
+      name: 20,
+      value: 0,
+    },
+    {
+      name: 21,
+      value: 0,
+    },
+    {
+      name: 22,
+      value: 0,
+    },
+    {
+      name: 23,
+      value: 0,
+    },
+    {
+      name: 24,
+      value: 0,
+    },
+    {
+      name: 25,
+      value: 0,
+    },
+    {
+      name: 26,
+      value: 0,
+    },
+    {
+      name: 27,
+      value: 0,
+    },
+  ]);
   const goToBlock = () => props.navigation.navigate("Block");
-  const user = JSON.parse(localStorage.getItem('user') || '').user_name.name
-  
 
+  useEffect(() => {
+    const getBlocks = async () => {
+      try {
+        setUser( JSON.parse(await AsyncStorage.getItem('user'||'')|| '').user.name )
+        setLoading(true);
+        const res = await getSelectedBlocks();
 
-    const arr:any = []
-    for(let i = 0 ; i <10 ; i++){
-        arr.push(
-            <ProfileBlocks
-             id={i}
-              onPress={ goToBlock}
-              />
-        )
-    }
+        setBlocks(
+          blocks.map((block: any, index: number) => {
+            return res?.data?.data.find(
+              (selected: any) => selected.name == block.name
+            )
+              ? {
+                  name: block.name,
+                  value: 1,
+                }
+              : block;
+          })
+        );
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getBlocks();
+  }, []);
+
   return (
     <ScrollView>
-      <View >
+      <View>
         <View style={styles.header}>
-            
-        <Image source={require('../../../assets/images/avatar.jpg')} style={{width:70, height:70, borderRadius:100, margin:20, marginRight:0}}/>
-        <Text style={styles.userName}>{user}</Text>
-        <Btn text="Edit" onPress={goEditProfile} style={styles.editProBtn}/>
+          <Image
+            source={require("../../../assets/images/avatar.jpg")}
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: 100,
+              margin: 20,
+              marginRight: 0,
+            }}
+          />
+          <Text style={styles.userName}>{user}</Text>
+          <Btn text="Edit" onPress={goEditProfile} style={styles.editProBtn} />
         </View>
-        <Btn text="Edit Sketch" onPress={goToSketch} style={styles.editBtn}/>
-        <View style={styles.container}>{arr}</View>
-        
+        <Btn text="Edit Sketch" onPress={goToSketch} style={styles.editBtn} />
+        <View style={styles.container}>
+          {loading?<Loading/>:blocks.map((block: any) => (
+            <ProfileBlocks
+              name={block.name}
+              value={block.value}
+              onPress={goToBlock}
+            />
+          ))}
+        </View>
+
         {/* <Button title="BACK" onPress={goBackk}></Button> */}
       </View>
     </ScrollView>
   );
 };
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent:'center',
-        alignContent:'center',
-        marginBottom:7
-    },
-    editBtn:{
-        marginLeft:100,
-        marginRight:42,
-        marginBottom:10,
-        color:"white",
-        alignSelf:'flex-end'
-    },
-    editProBtn:{
-      maxWidth:100,
-      maxHeight:45,
-      margin:10,
-      marginTop:30,
-      marginLeft:30
-    },
-    titleTxt:{
-        margin:10,
-        marginBottom:13,
-        alignSelf:'center',
-        fontSize:32,
-        fontWeight:'bold',
-        
-    },
-    header:{
-      flexDirection:'row',
-      marginBottom:10,
-      justifyContent:'space-between'
-
-    },
-    userName:{
-      marginTop:38,
-      marginLeft:10,
-      fontSize:24,
-      fontWeight:'bold'
-    }
-
-  });
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignContent: "center",
+    marginBottom: 7,
+  },
+  editBtn: {
+    marginLeft: 100,
+    marginRight: 42,
+    marginBottom: 10,
+    color: "white",
+    alignSelf: "flex-end",
+  },
+  editProBtn: {
+    maxWidth: 100,
+    maxHeight: 45,
+    margin: 10,
+    marginTop: 30,
+    marginLeft: 30,
+  },
+  titleTxt: {
+    margin: 10,
+    marginBottom: 13,
+    alignSelf: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  header: {
+    flexDirection: "row",
+    marginBottom: 10,
+    justifyContent: "space-between",
+  },
+  userName: {
+    marginTop: 38,
+    marginLeft: 10,
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
 
 export default Profile;
