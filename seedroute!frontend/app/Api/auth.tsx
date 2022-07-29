2a79533917b295a172d57bc1a973515f25591271
ali-elmowed <almowed.ali@gmail.com>
2022-07-29 17:35:50 +0300

    Frontend: edited the auth apis

--- seedroute!frontend/app/Api/auth.tsx
+++ seedroute!frontend/app/Api/auth.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { baseURL } from './base';
 
 export interface loginProps {
     name?: string
@@ -6,14 +7,14 @@ export interface loginProps {
     password: string
   }
 export const login = async (email: string, password: string) => {
-    const res = axios.post("http://10.0.2.2:8000/api/login", {
+    const res = axios.post(`http://10.0.2.2:8000/api/login`, {
         email,
         password
     })
     return res
 }
 export const register = async (name: string, email: string, password: string, password_confirmation: string) => {
-    const res = axios.post("http:/10.0.2.2:8000/api/register", {
+    const res = axios.post(`http://10.0.2.2:8000/api/register`, {
         name,
         email,
         password,
