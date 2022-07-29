6db5591452873acf6f73c7e149d6906cb0992c57
ali-elmowed <almowed.ali@gmail.com>
2022-07-29 19:55:52 +0300

    Frontend: edited the styling of notification

--- seedroute!frontend/app/screens/Notification/Notification.screen.tsx
+++ seedroute!frontend/app/screens/Notification/Notification.screen.tsx
@@ -27,13 +27,14 @@ const Notifications = (props: homeScreenProps) => {
 const styles = StyleSheet.create({
   icon: {
     color: "#0D9B81",
-    margin:30,
+    margin:25,
     padding: 14,
     
   },
   chatBar: {
     borderTopWidth: 1,
     borderBottomWidth: 1,
+    borderColor: "gray",
     flexDirection: "row",
   },
   text:{
