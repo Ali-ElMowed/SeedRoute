226e52d3d94354d9a97257560aee0429dc60f266
ali-elmowed <almowed.ali@gmail.com>
2022-07-29 19:59:17 +0300

    Frontend: Used async storage instead of local

--- seedroute!frontend/app/Api/blocks.tsx
+++ seedroute!frontend/app/Api/blocks.tsx
@@ -1,23 +1,27 @@
 import axios from "axios";
+import { useAppSelector } from "../redux/hooks";
 import { baseURL } from "./base";
-const user = JSON?.parse(localStorage.getItem('user') || '{}')?.access_token
+import { AsyncStorageStatic } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+// const user = JSON?.parse(localStorage.getItem('user') || '{}')?.access_token
 
 
 export const postSelectedBlocks = async (data: any) => {
-    console.log(user);
-    
+ const user:any = JSON.parse(await AsyncStorage.getItem('user')||'')
     const res = await axios.post(`http://10.0.2.2:8000/api/storeBlocksSelected`,data,{
         headers: {
-            Authorization :  `Bearer ${user}`
+            Authorization :  `Bearer ${user?.access_token}`
         }
     });
     return res;
 }
 
 export const getSelectedBlocks = async () => {
+    const user:any = JSON.parse(await AsyncStorage.getItem('user')||'')
+    
     const res = await axios.get(`http://10.0.2.2:8000/api/getSelectedBlocks`,{
         headers: {
-            Authorization :  `Bearer ${user}`
+            Authorization :  `Bearer ${user?.access_token}`
         }
     });
     return res;
