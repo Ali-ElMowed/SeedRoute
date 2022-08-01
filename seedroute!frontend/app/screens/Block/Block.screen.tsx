38264d3089802d4ec374c793fb0384916b74d9f0
ali-elmowed <almowed.ali@gmail.com>
2022-08-01 14:42:27 +0300

    Frontend: Formated the page

--- seedroute!frontend/app/screens/Block/Block.screen.tsx
+++ seedroute!frontend/app/screens/Block/Block.screen.tsx
@@ -2,73 +2,101 @@ import { useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
 import { getSelectedBlockByName } from "../../Api/blocks";
+import { getPlantById } from "../../Api/plants";
 import Btn from "../../Components/Btn";
+import Loading from "../../Components/Loading";
 interface homeScreenProps {
   navigation: any;
-  name: any
+  name: any;
 }
 const Block = (props: homeScreenProps) => {
-  const route = useRoute();
-
-  const [_block, setBlock]: any = useState(null);
-
-  useEffect(() => {
-    const getData = async (blockName: number) => {
-      try {
-        const { blockName }: any = route.params;
-        const blockByName = await getSelectedBlockByName(blockName);
-        setBlock(blockByName?.data?.data);
-        console.log(blockByName?.data?.data);
-        
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getData(props.name);
-  }, []);
-
   
 
-  
 
-const goToWatering = () => props.navigation.navigate('Watering')
-const doPlant = () => props.navigation.navigate('doPlant')
+
+
+  
+ 
+  const goToWatering = () => props.navigation.navigate("Watering");
+  const doPlant = () => props.navigation.navigate("doPlant");
+  const plantedAt = new Date(_block.planted_at);
+ 
   return (
     <ScrollView>
-      <View >
-        <Btn text="watering" onPress={goToWatering} style={styles.btn}/>
-        <Text style={styles.blockName}>This is block number {_block?.name}</Text>
+      {loading ? (
+        <Loading />
+      ) : (
         <View>
-          {_block?.plant_id === null ? <View>
-            <Text style={styles.noplant}> This Block is ready To be Planted</Text>
-            <Btn text="Plant" style={styles.btn} onPress={doPlant}/>
-          </View> 
-          : 
-          <Text>This is block has {_block?.plant_id}</Text>}
+          <Btn text="watering" onPress={goToWatering} style={styles.btn} />
+          <Text style={styles.blockName}>
+            This is block number {_block?.name}
+          </Text>
+
+          <View>
+            {_block?.plant_id === null ? (
+              <View>
+                <Text style={styles.noplant}>
+                  This Block is ready To be Planted
+                </Text>
+                <Btn text="Plant" style={styles.btn} onPress={doPlant} />
+              </View>
+            ) : (
+              <>
+                <Image
+                  source={require("../../../assets/images/potato.jpeg")}
+                  style={styles.img}
+                />
+                <Text style={styles.yesPlant}>Plant: {_plant?.name}</Text>
+                <Text style={styles.phase}>
+                  {" "}
+                  <Text style={styles.phaseTitle}>Phase:</Text> {_block?.phase}{" "}
+                  of {_plant?.number_of_phases}
+                </Text>
+                <Text style={styles.phase}>
+                  {" "}
+                  <Text style={styles.phaseTitle}>Planted on:</Text>{" "}
+                  {_block?.planted_at}
+                </Text>
+                <Text style={styles.phase}>
+                  {" "}
+                  <Text style={styles.phaseTitle}>Growth Duration:</Text>{" "}
+                  {_plant?.growth_duration} days
+                </Text>
+                <Text style={styles.phase}>
+                  {" "}
+                  <Text style={styles.phaseTitle}>Expected Harvest :</Text>{" "}
+                  {_block?.planted_at + _plant?.growth_duration}
+                </Text>
+              </>
+            )}
+          </View>
         </View>
-      </View>
+      )}
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-btn:{
-    marginTop:30,
-    width:200,
-    alignSelf:'center',
-},
-blockName:{
-  margin:20,
-  fontSize:18,
-  marginTop:30,
-  alignSelf:'center'
-},
-noplant:{
-  fontSize:22,
-  alignSelf:'center',
-  marginTop: 100
-
-},
-
-})
+  img: {
+   
+  },
+  btn: {
+   
+  },
+  blockName: {
+    
+  },
+  noplant: {
+    
+  },
+  yesPlant: {
+    
+  },
+  phase: {
+   
+  },
+  phaseTitle: {
+    
+  },
+});
 export default Block;
