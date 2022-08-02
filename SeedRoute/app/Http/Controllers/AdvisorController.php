3cf706b3204839333f339e7b6c77d8cdd449f37c
ali-elmowed <almowed.ali@gmail.com>
2022-08-02 11:20:52 +0300

    Backend: Added getAdvisors api

--- SeedRoute/app/Http/Controllers/AdvisorController.php
+++ SeedRoute/app/Http/Controllers/AdvisorController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdvisorController extends Controller
 {
     //
+    public function getAdvisors($id = null)
+    {
+        if ($id != null) {
+            $advisor = User::where('is_advisor','=', 1)->where('id','=',$id)->first();
+        } else {
+            $advisor = User::where('is_advisor','=', 1)->get();
+        }
+
+        return jsonResponse("plants found", 200, $advisor);
+    }
 }
