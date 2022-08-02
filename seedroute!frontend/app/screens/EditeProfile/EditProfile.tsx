4a8841752c86410611e429c5323b5c4e55b4d9d8
ali-elmowed <almowed.ali@gmail.com>
2022-08-03 02:25:20 +0300

    Frontend: Added imagePicker

--- seedroute!frontend/app/screens/EditeProfile/EditProfile.tsx
+++ seedroute!frontend/app/screens/EditeProfile/EditProfile.tsx
@@ -1,16 +1,21 @@
+//ts
 import React, { useEffect, useState } from "react";
 import {
   Text,
   View,
   StyleSheet,
-  Image,
   ScrollView,
   Pressable,
   TouchableOpacity,
-  TextInput
-
+  TextInput,
+  Platform,
+  Button,
+  Image,
 } from "react-native";
 import { profile } from "../../Api/auth";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+
 interface homeScreenProps {
   navigation: any;
 }
@@ -19,11 +24,11 @@ const EditProfile = (props: homeScreenProps) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [verifyPassword, setVerifyPassword] = useState("");
-  const [_profile, setProfile]:any = useState(null)
-  const [loading ,setLoading] = useState(false)
-  const changeImg = () => {
+  const [_profile, setProfile]: any = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState(null);
 
-  }
+  
 
   useEffect(() => {
     const getData = async () => {
@@ -32,7 +37,6 @@ const EditProfile = (props: homeScreenProps) => {
         setProfile(getProfile?.data);
         setLoading(true);
         console.log(getProfile?.data);
-        
       } catch (error) {
         console.log(error);
       } finally {
@@ -42,32 +46,48 @@ const EditProfile = (props: homeScreenProps) => {
     getData();
   }, []);
 
+  const pickImage = async () => {
+    let result : any = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+      console.log("++++++0"+image);
+      
+    }
+  };
+
+  
 
   return (
     <ScrollView>
-      <View>
-        <Image
-          source={require("../../../assets/images/avatar.jpg")}
-          style={styles.img}
-        />
-
-        <Pressable>
+    
+    {image && <Image source={{ uri: image }} style={{ width: 200, height: 200,alignSelf: 'center', borderRadius: 100, marginTop: 30,}} />}
+        <Pressable onPress={pickImage}
+        >
           <Text style={styles.changetext}>Change Profile Picture</Text>
         </Pressable>
         <Text style={styles.titles}>Name</Text>
-        <TextInput style={styles.input} onChangeText={setName} value={_profile?.name }/>
+        <TextInput
+          style={styles.input}
+          onChangeText={setName}
+          value={_profile?.name}
+        />
         <Text style={styles.titles}>Email</Text>
-        <TextInput style={styles.input} onChangeText={setEmail} value={_profile?.email}/>
-        {/* <Text style={styles.titles}>Password</Text>
-        <TextInput style={styles.input} onChangeText={setPassword} secureTextEntry />
-        <Text style={styles.titles}>Verify Password</Text>
-        <TextInput style={styles.input} onChangeText={setVerifyPassword} secureTextEntry/> */}
-        <Pressable onPress={changeImg}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={_profile?.email}
+        />
+        <Pressable>
           <Text style={styles.changetext}>Update</Text>
         </Pressable>
-        
-      </View>
     </ScrollView>
   );
 };
@@ -101,14 +121,14 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "white",
     borderRadius: 6,
-    padding:6,
+    padding: 6,
     backgroundColor: "white",
-    paddingLeft:15
+    paddingLeft: 15,
   },
-  titles:{
-    marginLeft:30,
-    marginBottom:5
+  titles: {
+    marginLeft: 30,
+    marginBottom: 5,
   },
-
+  
 });
 export default EditProfile;
