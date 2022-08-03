39240bb86c073defab9cee8d2a19dd3ca2d456bf
ali-elmowed <almowed.ali@gmail.com>
2022-08-03 15:23:13 +0300

    Frontend: Edited the style of image bcz of error

--- seedroute!frontend/app/screens/Plant/Plant.screen.tsx
+++ seedroute!frontend/app/screens/Plant/Plant.screen.tsx
@@ -11,6 +11,7 @@ const Plant = (props: homeScreenProps) => {
   const route = useRoute();
 
   const [_plant, setPlant]: any = useState(null);
+  const [_image, setImage]: any = useState(null);
 
   useEffect(() => {
     const getData = async (id: number) => {
@@ -18,6 +19,10 @@ const Plant = (props: homeScreenProps) => {
         const { id }: any = route.params;
         const plantById = await getPlantById(id);
         setPlant(plantById?.data?.data);
+        const image = plantById?.data?.data?.image
+        setImage(image)
+        console.log(image);
+        
       } catch (error) {
         console.log(error);
       }
@@ -27,33 +32,39 @@ const Plant = (props: homeScreenProps) => {
 
   return (
     <ScrollView>
-      {
-        _plant ? <>
-        
-      <View style={styles.container}>
-        <Image
-          source={require("../../../assets/images/potato.jpeg")}
-          style={styles.img}
-        />
-        <Text style={styles.plant_name}>{_plant?.name}</Text>
-        <View style={styles.info}>
-          <Text style={styles.secTitle}>General Information :-</Text>
-          <Text style={styles.secContent}>&#8226;
-            Growth Duration: {_plant?.growth_duration} days
-          </Text>
-          <Text style={styles.secContent}>&#8226; Plant Type: {_plant?.type}</Text>
-          <Text style={styles.secContent}>&#8226; Soil Type: {_plant?.soil_type}</Text>
-          <Text style={styles.secContent}>&#8226; 
-            The plant go through {_plant?.number_of_phases} phases
-          </Text>
+      {_plant ? (
+        <>
+          <View style={styles.container}>
+            <Image
+              source={{ uri: `http://10.0.2.2:8000/storage/${_image}`}}
+              style={{width: 250, height: 250, resizeMode: "contain", alignSelf: "center", borderRadius: 6,marginTop: 40,}}
+            /> 
+            <Text style={styles.plant_name}>{_plant?.name}</Text>
+            <View style={styles.info}>
+              <Text style={styles.secTitle}>General Information :-</Text>
+              <Text style={styles.secContent}>
+                &#8226; Growth Duration: {_plant?.growth_duration} days
+              </Text>
+              <Text style={styles.secContent}>
+                &#8226; Plant Type: {_plant?.type}
+              </Text>
+              <Text style={styles.secContent}>
+                &#8226; Soil Type: {_plant?.soil_type}
+              </Text>
+              <Text style={styles.secContent}>
+                &#8226; The plant go through {_plant?.number_of_phases} phases
+              </Text>
 
-          <Text style={styles.secTitle}>Recommendations :-</Text>
-          <Text style={styles.secContent}>&#8226; 
-            Watering {_plant?.watering} per week
-          </Text>
-        </View>
-      </View></> : <Loading/>
-      }
+              <Text style={styles.secTitle}>Recommendations :-</Text>
+              <Text style={styles.secContent}>
+                &#8226; Watering {_plant?.watering} per week
+              </Text>
+            </View>
+          </View>
+        </>
+      ) : (
+        <Loading />
+      )}
     </ScrollView>
   );
 };
@@ -78,7 +89,7 @@ const styles = StyleSheet.create({
   secTitle: {
     margin: 10,
     marginLeft: 20,
-    marginTop:15,
+    marginTop: 15,
     fontSize: 20,
     fontWeight: "bold",
     color: "#4D544D",
