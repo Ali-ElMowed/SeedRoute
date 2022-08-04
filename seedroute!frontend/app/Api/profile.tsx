eff9f18598e2ee8087e1c6aeacd2c035871a4f34
ali-elmowed <almowed.ali@gmail.com>
2022-08-04 13:23:27 +0300

    Frontend: Edited the axios api

--- seedroute!frontend/app/Api/profile.tsx
+++ seedroute!frontend/app/Api/profile.tsx
@@ -2,14 +2,20 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { IsAny } from "@reduxjs/toolkit/dist/tsHelpers";
 import axios from "axios";
 
-export const updateUser = async (name: any, email:any, image:any) => {
+export const updateUser = async (
+    name:any,email:any,image:any
+    ) => {
     const user:any = JSON.parse(await AsyncStorage.getItem('user')||'')
-    const res = await axios.post(`http://10.0.2.2:8000/api/updateUser`,{"name":name , "email":email, "image":image},{
+    console.log("data");
+    
+    const res = await axios.put("http://10.0.2.2:8000/api/updateUser",{
+        name,email,image
+    },{
         headers: {
             Authorization :  `Bearer ${user?.access_token}`,
-            'Content-Type' : 'multipart/form-data'
-            
+            'content-type': 'application/x-www-form-urlencoded'  
         }
     });
+    
     return res;
 }
