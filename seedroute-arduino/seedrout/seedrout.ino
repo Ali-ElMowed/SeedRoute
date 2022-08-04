71bcb34fef697b40dfee796ece371f969816968b
ali-elmowed <almowed.ali@gmail.com>
2022-08-04 19:47:52 +0300

    Arduino: Changed the arduino code

--- seedroute-arduino/seedrout/seedrout.ino
+++ seedroute-arduino/seedrout/seedrout.ino
@@ -1,133 +1,47 @@
-// Include libraries needed
-#include < Arduino.h >
-#include " Adafruit_BLE.h "
-#include " Adafruit_BluefruitLE_UART.h "
-#include " BluefruitConfig.h "
-#define FACTORYRESET_ENABLE
-#define MINIMUM_FIRMWARE_VERSION " 0.6.6 "
-#define MODE_LED_BEHAVIOUR " MODE "
+
 #include <SoftwareSerial.h>
 SoftwareSerial SoftSerial(2, 3);
-
-int counterChannel;
-int elevationChannel;
-
-int counter = 0;
-
-SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
-Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
-                              BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
-
 const int RELAY_PIN = A5;
-#define POWER_PIN 7
-#define SIGNAL_PIN A5
+
 
 int value = 0;
-void setup()
-{
-  // initialize digital pin A5 as an output.
+int flag = 0;
+unsigned long lightStartTime;
+unsigned long currentTime;
+unsigned long interval = 5000;
+
+void setup() {
+  // put your setup code here, to run once:
   pinMode(RELAY_PIN, OUTPUT);
   Serial.begin(9600);
-  pinMode(POWER_PIN, OUTPUT);   // configure D7 pin as an OUTPUT
-  digitalWrite(POWER_PIN, LOW); // turn the sensor OFF
-  while (!Serial)
-  {
-    delay(500);
-  }
-  Serial.begin(115200);
+ 
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0){
+    char data = Serial.read();
 
-  if (!ble.begin(VERBOSE_MODE))
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+    switch(data){
+      case 'on': switchOn(20);
+            break;
+    }
+
   }
 
-  if (FACTORYRESET_ENABLE)
-  {
-    /* Perform a factory reset to make sure everything is in a known state */
-    Serial.println(F("Performing a factory reset: "));
-    if (!ble.factoryReset())
-    {
-      error(F("Couldn't factory reset"));
+  if(flag == 1){
+    currentTime = millis();
+    if(lightStartTime + interval < currentTime){
+      digitalWrite(RELAY_PIN,LOW);
+      flag = 0;
     }
   }
-
-  ble.echo(false);
-
-  Serial.println("Requesting Bluefruit info:");
-
-  ble.info();
-
-  ble.reset();
-
-  counterChannel = ble.println(F("AT+GATTADDCHAR=UUID=0x2A19,PROPERTIES=0x10,MIN_LEN=1,DESCRIPTION=Counter,VALUE=100"));
-  if (counterChannel == 0)
-  {
-    error(F("Error adding characteristic"));
-  }
-
-  elevationChannel = ble.println(F("AT+GATTADDCHAR=UUID=0x2A6C,PROPERTIES=0x08,MIN_LEN=1,DESCRIPTION=Elevation,VALUE=0"));
-  if (elevationChannel == 0)
-  {
-    error(F("Error adding characteristic"));
-  }
-
-  ble.reset();
-  Serial.println();
-
-  ble.verbose(false);
-
-  while (ble.isConnected() && counter= 3) {
-      digitalWrite(A2, HIGH);
-      counter ++
-  }
-  digitalWrite(A2, LOW);
+ 
 }
-
-// the loop function runs over and over again forever
-void loop()
+void switchOn(unsigned long timePeriod)
 {
-  digitalWrite(RELAY_PIN, HIGH); // turn on pump 5 seconds
-  delay(5000);
-  digitalWrite(RELAY_PIN, LOW); // turn off pump 5 seconds
-  delay(5000);
-
-  digitalWrite(POWER_PIN, HIGH);  // turn the sensor ON
-  delay(10);                      // wait 10 milliseconds
-  value = analogRead(SIGNAL_PIN); // read the analog value from sensor
-  digitalWrite(POWER_PIN, LOW);   // turn the sensor OFF
-
-  Serial.print("Sensor value: ");
-  Serial.println(value);
-
-  delay(1000);
-}
-const int RELAY_PIN = A5;
-#define POWER_PIN 7
-#define SIGNAL_PIN A5
-
-int value = 0;
-void setup()
-{
-  pinMode(RELAY_PIN, OUTPUT);
-  Serial.begin(9600);
-  pinMode(POWER_PIN, OUTPUT);
-  digitalWrite(POWER_PIN, LOW);
-}
-
-void loop()
-{
-  digitalWrite(RELAY_PIN, HIGH);
-  delay(5000);
-  digitalWrite(RELAY_PIN, LOW);
-  delay(5000);
-
-  digitalWrite(POWER_PIN, HIGH);
-  delay(10);
-  value = analogRead(SIGNAL_PIN);
-  digitalWrite(POWER_PIN, LOW);
-
-  Serial.print("Sensor value: ");
-  Serial.println(value);
-
-  delay(1000);
+  digitalWrite(LED_BUILTIN,HIGH);
+  lightStartTime = millis();
+  flag = 1;
+  interval = timePeriod*1000;
 }
